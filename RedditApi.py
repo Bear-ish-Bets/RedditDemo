124832b5286d0a3340e025a1d3ef18866a07cb9a
Trevor Westin <trevorwwestin@gmail.com>
2023-05-30 15:42:14 -0400

    test data complete

--- RedditApi.py
+++ RedditApi.py
@@ -1,4 +1,7 @@
+import json
 import praw
+
+
 
 # reddit = praw.Reddit(client_id='YOUR_CLIENT_ID',
 #                      client_secret='YOUR_CLIENT_SECRET',
@@ -11,16 +14,19 @@ reddit = praw.Reddit("BearishBetsBot")
 # Instantiate subreddit
 subreddit = reddit.subreddit('wallstreetbets')
 
-# print('Current Top 10 HOT r/wallstreetbets post:')
-# for submission in subreddit.hot(limit=10):
-#     print(submission.title)
 
-print('\nHottest Post to r/wallstreetbets:')
-for submission in subreddit.hot(limit=1):
-    print(f"Author: {submission.author}")
-    print(f"Score: {submission.score}")
-    print(f"Title: {submission.title}")
-    print(f"Description: {submission.selftext}")
-    print(f"URL: {submission.url}")
-    print(f"Number of comments: {submission.num_comments}")
-    print(f"Created (UTC): {submission.created_utc}\n")
+hot_posts = subreddit.hot(limit=1000)
+posts = []
+# Hottest Post in r/wallstreetbets
+for post in hot_posts:
+    posts.append({
+        'score': post.score,
+        'title': post.title,
+        'selftext': post.selftext,
+        'score': post.score,
+        'created_utc': post.created_utc,
+    })
+
+# Write to file
+with open('hot_posts.json', 'w') as f:
+    json.dump(posts, f, indent=4)
